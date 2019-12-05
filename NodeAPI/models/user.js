e6b4d9d0cfaccc40f54d3ce99944462e18661b4f
tanaymittal25 <tanay.mittal25@gmail.com>
2019-12-05 21:42:06 +0530

    Virtual

--- NodeAPI/models/user.js
+++ NodeAPI/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const uuidv1 = require('uuid/v1');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
 	name: {
@@ -22,5 +24,35 @@ const userSchema = new mongoose.Schema({
 	},
 	updated: Date
 });
+
+userSchema.virtual('password')
+	.set(function(password) {
+		//Temp variable for password
+		this._password = password;
+
+		//Salt Variable
+		this.salt = uuidv1();
+
+		//Encrypt Password
+		this.hashed_password = this.encryptPassword(password);
+
+	})
+	.get(function() {
+		return this._password;
+	});
+
+userSchema.methods = {
+	encryptPassword: function(password) {
+		if (!password) return "";
+		try {
+			return crypto.createHmac('sha1', this.salt)
+				.update(password)
+				.digest('hex');
+		} catch (err) {
+			return "";
+		}
+	}
+}
+
 
 module.exports = mongoose.model("User", userSchema);
