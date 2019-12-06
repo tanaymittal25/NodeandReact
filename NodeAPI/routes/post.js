0e562bf0b13b474803e6d484143660970eab46a2
tanaymittal25 <tanay.mittal25@gmail.com>
2019-12-06 15:44:29 +0530

    userId

--- NodeAPI/routes/post.js
+++ NodeAPI/routes/post.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { getPost, createPost } = require("../controllers/post");
 const { requireSignIn } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 const validator = require("../validators/index");
 
 const router = express.Router();
 
 router.get("/", requireSignIn, getPost);
 router.post("/post", validator.createPostValidator, createPost);
+
+router.param("userId", userById);
 
 module.exports = router;
