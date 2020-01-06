763f0b6c8644a0fd2af35911a59a30926e7d0e70
tanaymittal25 <tanay.mittal25@gmail.com>
2020-01-06 19:44:27 +0530

    Create Post

--- NodeAPI/controllers/post.js
+++ NodeAPI/controllers/post.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const formidable = require('formidable');
+const fs = require('fs');
 
 exports.getPost = ( req, res ) => {
     const posts = Post.find()
@@ -9,12 +11,30 @@ exports.getPost = ( req, res ) => {
     	.catch( err => console.log(err));
 };
 
-exports.createPost = (req, res) => {
-	const post = new Post(req.body);
+exports.createPost = (req, res, next) => {
+	let form = new formidable.IncomingForm();
+	form.keepExtensions = true;
+	form.parse(req, (err, fields, files) => {
+		if (err) {
+			return res.status(400).json({
+				error: "Photo not parsed"
+			});
+		}
 
-	post.save().then(result => {
-		return res.json({
-			post: result
+		let post = new Post(fields);
+		post.postedBy = req.profile;
+		if (files.photo) {
+			post.photo.data = fs.readFileSync(files.photo.path);
+			post.photo.contentType = files.photo.type;
+		}
+
+		post.save((err, result) => {
+			if (err) {
+				return res.status(400).json({
+					error: err
+				});
+			}
+			res.json(result);
 		});
 	});
 };
