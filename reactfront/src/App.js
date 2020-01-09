ba2b99f3a17d49b424558b1f3b9ddad328c455fc
tanaymittal25 <tanay.mittal25@gmail.com>
2020-01-09 11:40:34 +0530

    Homepage

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import MainRouter from './MainRouter';
 
 const App = () => (
-  <BrowserRouter>
-    <MainRouter />
-  </BrowserRouter>
+  <div>
+    <BrowserRouter>
+      <MainRouter />
+    </BrowserRouter>
+  </div>
 );
 
 export default App;
