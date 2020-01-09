32172463dd1735ed0df906cbdca8563af0482dbf
tanaymittal25 <tanay.mittal25@gmail.com>
2020-01-09 14:04:07 +0530

    Signup response

--- reactfront/src/user/Signup.js
+++ reactfront/src/user/Signup.js
@@ -16,6 +16,42 @@ class Signup extends Component {
         this.setState({ [name]: event.target.value });
     }
 
+    clickSubmit = event => {
+        event.preventDefault();
+        const { name, email, password } = this.state;
+        const user = {
+            name,
+            email,
+            password
+        }
+
+        this.signup(user)
+            .then(data => {
+                if (data.error) this.setState({ error: data.error })
+                else this.setState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    error: ""
+                });
+            });
+    };
+
+    signup = (user) => {
+        return fetch("http://localhost:8080/signup", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         const { name, email, password } = this.state;
         return (
@@ -50,7 +86,7 @@ class Signup extends Component {
                             value={password}
                         />
                     </div>
-                    <button className="btn btn-raised btn-primary">SUBMIT</button>
+                    <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">SUBMIT</button>
                 </form>
             </div>
         );
