e2000c75304f58b93de554fedc5355a3afa33b33
tanaymittal25 <tanay.mittal25@gmail.com>
2020-01-09 10:53:52 +0530

    HomePage

---
+++ reactfront/src/core/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => (
+    <div className="jumbotron">
+        <h2>Home</h2>
+        <p className="lead">
+            Welcome
+        </p>
+    </div>
+);
+
+export default Home;
