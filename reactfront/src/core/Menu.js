ab722bd2bed489f4785f4e21c2da34abcb9e2776
tanaymittal25 <tanay.mittal25@gmail.com>
2020-01-13 09:48:58 +0530

    Menu

--- reactfront/src/core/Menu.js
+++ reactfront/src/core/Menu.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-const Menu = () => (
+const isActive = (history, path) => {
+	if (history.location.pathname === path) return { color: "#ff9900" };
+	else return { color: "#ffffff" }; 
+}
+
+const Menu = ({ history }) => (
 	<div>
-		<Link to='/'> Home </Link>
-		<Link to='/signin'> Sign In </Link>
-		<Link to='/signup'> Sign Up </Link>
+		<ul className="nav nav-tabs bg-primary">
+			<li className="nav-item">
+				<Link className="nav-link" style={ isActive(history, '/') } to='/'> Home </Link>
+			</li>
+			<li className="nav-item">
+				<Link className="nav-link" style={ isActive(history, '/signin') } to='/signin'> Sign In </Link>
+			</li>
+			<li className="nav-item">
+				<Link className="nav-link" style={ isActive(history, '/signup') } to='/signup'> Sign Up </Link>
+			</li>
+		</ul>
 	</div>
 );
 
-export default Menu;
+export default withRouter(Menu);
