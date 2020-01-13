98c931f944c78e9d9f2c061ff474a25e78946a0b
tanaymittal25 <tanay.mittal25@gmail.com>
2020-01-13 12:19:10 +0530

    Signout

--- reactfront/src/core/Menu.js
+++ reactfront/src/core/Menu.js
@@ -6,18 +6,68 @@ const isActive = (history, path) => {
 	else return { color: "#ffffff" }; 
 }
 
+export const signout = (next) => {
+	if (typeof window !== "undefined") localStorage.removeItem("jwt");
+	next();
+	return fetch("http://localhost:8180/signout", {
+		method: "GET"
+	})
+		.then(response => {
+			console.log(response);
+			return response.json();
+		})
+		.catch(err => console.log(err));
+};
+
+export const isAuthenticated = () => {
+	if (typeof window == "undefined")
+		return false;
+
+	if (localStorage.getItem("jwt"))
+		return JSON.parse(localStorage.getItem("jwt"));
+	else
+		return false;
+};
+
 const Menu = ({ history }) => (
 	<div>
 		<ul className="nav nav-tabs bg-primary">
 			<li className="nav-item">
 				<Link className="nav-link" style={ isActive(history, '/') } to='/'> Home </Link>
 			</li>
-			<li className="nav-item">
-				<Link className="nav-link" style={ isActive(history, '/signin') } to='/signin'> Sign In </Link>
-			</li>
-			<li className="nav-item">
-				<Link className="nav-link" style={ isActive(history, '/signup') } to='/signup'> Sign Up </Link>
-			</li>
+			{
+				!isAuthenticated() && (
+					<>
+						<li className="nav-item">
+							<Link className="nav-link" style={ isActive(history, '/signin') } to='/signin'> Sign In </Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" style={ isActive(history, '/signup') } to='/signup'> Sign Up </Link>
+						</li>
+					</>
+				)
+			}
+			{
+				isAuthenticated() && (
+					<>
+						<li className="nav-item">
+							<a 
+								className="nav-link" 
+								style={ isActive(history, '/signup'), { cursor: "pointer" } } 
+								onClick={() => signout(() => history.push('/'))} 
+							>
+								Sign Out 
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link">
+								{ isAuthenticated().user.name }
+							</a>
+						</li>
+					</>
+				)
+			}
+			
 		</ul>
 	</div>
 );
