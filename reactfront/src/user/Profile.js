5e48051d2bd1b679813a2ff053f21364ba1f0861
tanaymittal25 <tanay.mittal25@gmail.com>
2020-01-16 11:06:34 +0530

    Path Config

--- reactfront/src/user/Profile.js
+++ reactfront/src/user/Profile.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { isAuthenticated } from '../auth';
 
 class Profile extends Component {
-    render() {
-        return (
-            <div className="container">
-                <h2 className="mt-5 mb-5">
-                    Profile
-                </h2>
-            </div>
-        );
-    }
+
+  constructor() {
+    super();
+    this.state = {
+      user: "",
+      redirectUser: false
+    };
+  }
+
+  // componentDidMount() {
+  //   const userId = this.props.
+  // }
+
+  render() {
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">
+          Profile
+        </h2>
+        <p>Hello {isAuthenticated().user.name}</p>
+        <p>{isAuthenticated().user.email}</p>
+      </div>
+    );
+  }
 }
 
 export default Profile;
