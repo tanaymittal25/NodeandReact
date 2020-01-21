03ac61999de25903283500469892a5a66a590cf3
tanaymittal25 <tanay.mittal25@gmail.com>
2020-01-21 10:41:26 +0530

    Profile Page

--- reactfront/src/user/Profile.js
+++ reactfront/src/user/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { isAuthenticated } from '../auth';
+import { Redirect } from 'react-router-dom';
 
 class Profile extends Component {
 
@@ -11,11 +12,31 @@ class Profile extends Component {
     };
   }
 
-  // componentDidMount() {
-  //   const userId = this.props.
-  // }
+  componentDidMount() {
+    const userId = this.props.match.params.userId;
+    fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${isAuthenticated().token}`
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        if (data.error) {
+          this.setState({ redirectUser: true });
+        } else {
+          this.setState({ user: data });
+        }
+      });
+  }
 
   render() {
+    const redirectUser = this.state.redirectUser;
+    if (redirectUser) return <Redirect to="/singin" />
     return (
       <div className="container">
         <h2 className="mt-5 mb-5">
@@ -23,6 +44,9 @@ class Profile extends Component {
         </h2>
         <p>Hello {isAuthenticated().user.name}</p>
         <p>{isAuthenticated().user.email}</p>
+        <p>{`Joined ${new Date(
+          this.state.user.created
+        ).toDateString()}`}</p>
       </div>
     );
   }
